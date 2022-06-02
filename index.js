4aa3733354fc05309bb0ecb4a1d6cb791e5335b7
zadigg <62830716+zadigg@users.noreply.github.com>
2022-06-02 14:33:15 +0300

    final check 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
-const { MONGODB } = require("./config.js");
+// const { MONGODB } = require("./config.js");
 
 const pubsub = new PubSub();
 
