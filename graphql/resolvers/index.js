6959643461880a4dbbf9bfd28ec6ff46da66c8d3
zadigg <62830716+zadigg@users.noreply.github.com>
2022-06-02 13:20:32 +0300

    check if there is deleted file

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,21 @@
+const postsResolvers = require("./posts");
+const userResolvers = require("./users");
+const commentsResolvers = require("./comments");
+
+module.exports = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length,
+  },
+  Query: {
+    ...postsResolvers.Query,
+  },
+  Mutation: {
+    ...userResolvers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation,
+  },
+  Subscription: {
+    ...postsResolvers.Subscription,
+  },
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require("apollo-server");
+const { PubSub } = require("graphql-subscriptions");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(MONGODB, { useNewURLParser: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server is running on port ${res.url}`);
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
